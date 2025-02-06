62645835a77e002cdaec158f155cc6c2e811cfce
Blue Rogue <info@jozsefsimco.com>
2025-02-06 16:01:58 +0100

    menu improvement inside authenticated layout

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,10 +1,13 @@
 "use client";
 import { navLinks } from '@/constants';
-import { SignedIn } from '@clerk/nextjs';
+import { SignedIn, SignedOut } from '@clerk/nextjs';
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
+import { Button } from '@/components/ui/Button'
+
+
 
 const Sidebar = () => {
 
@@ -25,12 +28,28 @@ const Sidebar = () => {
 
                                 return (
                                     <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'}`}>
-                                        {link.label}
+                                        <Link className="sidebar-link" href={link.route}>
+                                            <Image
+                                                src={link.icon}
+                                                alt="logo"
+                                                width={24}
+                                                height={24}
+                                                className={`${isActive && 'brightness-200'}`}
+                                            />
+                                            {link.label}
+                                        </Link>
                                     </li>
                                 )
                             })}
                         </ul>
                     </SignedIn>
+                    
+                    <SignedOut>
+                        <Button asChild className="button bg-purple-gradient bg-cover">
+                            <Link href="/sign-in">Log In</Link>
+                        </Button>
+                    </SignedOut>
+                    
                 </nav>
             </div>
         </aside>
