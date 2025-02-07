2ede5abd7b9cc504cb210b8acef973d43453a2a7
Blue Rogue <info@jozsefsimco.com>
2025-02-08 00:28:41 +0100

    mongodb schema

---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,44 @@
+import {Schema, model, models } from "mongoose"; 
+
+
+// Schema 
+const UserSchema = new Schema ({
+    clerkId: { 
+        type: String,
+        required: true, 
+        unique: true,
+    },
+    email: { 
+        type: String,
+        required: true,
+        unique: true,
+    },
+    username: { 
+        type: String, 
+        required: true,
+        unique: true
+    }, 
+    photo: { 
+        type: String,
+        required: true
+    }, 
+    firstName: { 
+        type: String
+    }, 
+    lastName: { 
+        type: String
+    },
+    planId: { 
+        type: Number,
+        default: 1,
+     }, 
+    creditBalance: { 
+        type: Number, 
+        default: 10,
+    } 
+})
+
+// Model
+const User = models?.User || model('User', UserSchema); 
+
+export default User; 
